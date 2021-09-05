105b291404fc4ddac1cda30498a077ffc7eb3abc
Mohsin <mohsinali7595@gmail.com>
2021-09-05 09:41:01 +0530

    initial commit

---
+++ com/uniliver/target_data_loading.py
@@ -0,0 +1,52 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+import os.path
+import yaml
+import utils.apps_utils as ut
+
+
+if __name__ == '__main__':
+
+    os.environ["PYSPARK_SUBMIT_ARGS"] = (
+        '--packages "org.apache.hadoop:hadoop-aws:2.7.4" pyspark-shell'
+    )
+
+
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    app_config_path = os.path.abspath(current_dir + "/../../" + "application.yml")
+    app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
+
+    conf = open(app_config_path)
+    app_conf = yaml.load(conf, Loader=yaml.FullLoader)
+    secret = open(app_secrets_path)
+    app_secret = yaml.load(secret, Loader=yaml.FullLoader)
+
+    # Create the SparkSession
+    spark = SparkSession \
+        .builder \
+        .appName("DSL examples") \
+        .master('local[*]') \
+        .config("spark.mongodb.input.uri", app_secret["mongodb_config"]["uri"]) \
+        .getOrCreate()
+    spark.sparkContext.setLogLevel('ERROR')
+
+    # Setup spark to use s3
+    hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
+    hadoop_conf.set("fs.s3a.access.key", app_secret["s3_conf"]["access_key"])
+    hadoop_conf.set("fs.s3a.secret.key", app_secret["s3_conf"]["secret_access_key"])
+
+    cp_df = spark.read \
+        .option("header", "true") \
+        .option("delimiter", "|") \
+        .format("csv") \
+        .option("inferSchema", "true") \
+        .load(
+        "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["s3_conf"]["staging_dir"] + "/CP") \
+        .filter("run_dt=", current_date())
+
+
+    cp_df.show()
+
+    # spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4,mysql:mysql-connector-java:8.0.15,com.springml:spark-sftp_2.11:1.1.1,org.mongodb.spark:mongo-spark-connector_2.11:2.4.1" com/uniliver/target_data_loading.py
+
+
